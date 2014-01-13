c965d53dac4d564f3cf3951e6466b487318fcf44
Shawn Oakley <shawnoakley@gmail.com>
2014-01-12 21:05:43 -0500

    Properly create hash which generates legislator net wealth to per capita population net wealth comparison hash by state.

--- data_parser.py
+++ data_parser.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*- 
 import json
 import pandas as pd
 import re
 import unicodedata
+import numpy as np
+import scipy.sparse as sparse
 
 with open('pop_wealth_2010.json', 'r') as f:
      read_data = f.read()
@@ -12,6 +15,8 @@ pop_wealth_df = pd.read_json(read_data)
 pop_wealth_df.columns = ['State', 'Per Capita Net Wealth']
 pop_wealth = json.loads(read_data)
 
+print pop_wealth_df['State']
+
 with open('crp_data_2010.json', 'r') as f:
      read_data = f.read()
      f.closed
@@ -21,12 +26,11 @@ leg_wealth = json.loads(read_data)['records']
 # leg_wealth_df.columns = ['State', 'Legislator Net Wealth']
 # print leg_wealth
 
+
 # # print leg_wealth_df
 # print pop_wealth_df
 
 regex = re.compile("\([R|D]-(.+)\)")
-
-print regex
 
 names = []
 
@@ -64,7 +68,7 @@ a, b = abbrs.index('Nev'), abbrs.index('ND')
 abbrs[b], abbrs[a] = abbrs[a], abbrs[b]
 
 a = abbrs.index('Wis')
-abbrs[a] = 'Wy'
+abbrs[a] = 'Wyo'
 
 a = abbrs.index('Wash')
 abbrs[a] = 'Wis'
@@ -76,9 +80,50 @@ a = abbrs.index('VI')
 abbrs[a] = 'Vt'
 
 leg_state_dict = dict(zip(abbrs, state_strings))
-print leg_state_dict
-print len(leg_state_dict)
+# print leg_state_dict
+# print len(leg_state_dict)
 
+# print leg_wealth
 
+# for entry in pop_wealth:
+# 	entry['Leg_Wealth'] = []
 
+leg_wealth_hash = {state:0 for state in state_strings}
+leg_number_hash = {state:0 for state in state_strings}
 
+print leg_wealth_hash
+print leg_number_hash
+
+pop_wealth_df['Leg_Wealth'] = [0 for i in xrange(len(pop_wealth_df))]
+pop_wealth_df['Leg_Number'] = [0 for i in xrange(len(pop_wealth_df))]
+
+for entry in leg_wealth:
+	hit = regex.search(entry['Name'])
+	if hit is not None:
+		if hit.group(0)[3:-1] == 'Guam' or  hit.group(0)[3:-1] == 'DC' or  hit.group(0)[3:-1] == 'VI'  or  hit.group(0)[3:-1] == 'AS':
+			pass
+		else:
+			leg_wealth_hash[leg_state_dict[hit.group(0)[3:-1]]] += float(entry['AvgValue'])
+			# leg_wealth_hash[leg_state_dict[hit.group(0)[3:-1]]] += float(entry['MaxValue'])
+			leg_number_hash[leg_state_dict[hit.group(0)[3:-1]]] += 1
+
+# print leg_wealth_hash
+# print leg_number_hash
+
+leg_average_hash = {state:0 for state in state_strings}
+
+for state in state_strings:
+	leg_average_hash[state] = leg_wealth_hash[state]/leg_number_hash[state]
+
+# print leg_average_hash
+
+comp_hash = {state:0 for state in state_strings}
+print pop_wealth
+
+for state in state_strings:
+	for entry in pop_wealth:
+		if str(entry['State']) == state:
+			if entry["Wealth"] != 0:
+				comp_hash[state] = leg_average_hash[state] / entry['Wealth']
+
+print comp_hash
